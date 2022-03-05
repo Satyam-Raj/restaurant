f20cbf1172e0a10feaa6c26da0d55790060395f4
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2022-03-06 02:08:13 +0530

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,7 +32,7 @@
 
     
 
-    <v-main class="pt-7 grey lighten-1">
+    <v-main class="pt-3 grey lighten-1">
         
 
         <!--  -->
@@ -46,13 +46,12 @@
                 align="center"
             >
             <v-col
-                cols="6"
+                cols="12"
                 sm="4"
             >
                     <v-sheet
                         rounded="xl"
                         min-height="368"
-                        max-width="1500"
                         color="grey darken-2"
                         dark
                     >
@@ -77,13 +76,12 @@
                 
 
             <v-col
-                cols="6"
+                cols="12"
                 sm="4"
             >
                     <v-sheet
                         rounded="xl"
                         min-height="368"
-                        max-width="1500"
                         color="grey darken-2"
                         dark
                     >
@@ -107,44 +105,78 @@
             </v-row>
         </v-container>
 
+ 
+
+            
+            <v-container>
+                <v-row>
+               
+
+                <v-col
+                    cols="12"
+                    sm="12"
+                >
+                    <v-sheet
+                    min-height="40vh"
+                    rounded="lg"
+                    >
 
 
-        <div 
-            class="pa-7"
-            expansion-panel-content-padding
-        >
-
-        <h1 class="text-center">Frequently Asked Questions</h1>
-           
-        
-            <v-row justify="center" class="pa-7 mr-15 ml-15" >
-                <v-expansion-panels inset  focusable >
-
-                    <v-expansion-panel>
-                        <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >Why this technology is needed for merchant?</v-expansion-panel-header>
-                        <v-expansion-panel-content>
-                        This technology is developed to assist the merchant in managing their business. This is a management system that will help the merchant to keep track of their sales, inventory, and other business related information. This system will also help the merchant to manage their business in a more efficient way. Keep all the record (sales, inventory, etc.) of your business in one place and manage it online with ease.
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
-
-                    <v-expansion-panel>
-                        <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >Do we require to take subscription or is it free?</v-expansion-panel-header>
-                        <v-expansion-panel-content>
-                        This management system platform strictly requires subscription. This is because this system is developed by a team of developers and we have to pay for the technology which we are using to serve our customers like servers, domains, databases etc. However, we provide limited free trial period for our customers on special requests from their side, so that they can try out our system and see if it is suitable for them. 
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
-
-                    <v-expansion-panel>
-                        <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >Who can take advantage of this type of management system?</v-expansion-panel-header>
-                        <v-expansion-panel-content>
-                        A merchant who wants to manage their sales, inventory, and other business related information online. If you want to keep transactional records, sales, inventory, etc. of your business in one place, then this is the technology that you need. We provide all sorts of features to make your business management easier, and many more features are coming soon.
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
+                    <v-img
+                        src="https://www.clipartkey.com/mpngs/m/15-151495_icons-checklist-computer-black-text-white-check-list.png"
+                        max-width="20%"
+                    ></v-img>
+                        <h1>Hello</h1>
 
 
-                </v-expansion-panels>
-            </v-row>
-        </div>
+                    <!--  -->
+                    </v-sheet>
+                </v-col>
+
+                
+                </v-row>
+            </v-container>
+
+
+
+        <v-container>
+
+            <div 
+                class="pa-7 col-md-6 col-sm-12 mx-auto"          
+            >
+
+            <h1 class="text-center">Frequently Asked Questions</h1>
+            
+            
+                <v-row class="pa-7" :justify="center" >
+                    <v-expansion-panels inset  focusable>
+
+                        <v-expansion-panel>
+                            <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >How this technology is useful for merchant?</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                            This technology is developed to assist the merchant in managing their business. This is a management system that will help the merchant to keep track of their sales, inventory, and other business related information. This system will also help the merchant to manage their business in a more efficient way. Keep all the record (sales, inventory, etc.) of your business in one place and manage it online with ease.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+
+                        <v-expansion-panel>
+                            <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >Do we require to take subscription or is it free?</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                            This management system platform strictly requires subscription. This is because this system is developed by a team of developers and we have to pay for the technology which we are using to serve our customers like servers, domains, databases etc. However, we provide limited free trial period for our customers on special requests from their side, so that they can try out our system and see if it is suitable for them. 
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+
+                        <v-expansion-panel>
+                            <v-expansion-panel-header color="grey darken-2" class="white--text font-weight-bold" >Who can take advantage of this type of management system?</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                            A merchant who wants to manage their sales, inventory, and other business related information online. If you want to keep transactional records, sales, inventory, etc. of your business in one place, then this is the technology that you need. We provide all sorts of features to make your business management easier, and many more features are coming soon.
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+
+
+                    </v-expansion-panels>
+                </v-row>
+            </div>
+        </v-container>
        
 
     </v-main>
