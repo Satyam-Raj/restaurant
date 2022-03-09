9b3dbdede58c6e8e1ece95a29e0a3a8c509acf66
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2022-03-09 12:17:44 +0530

    login register

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -97,12 +97,14 @@
                                     </template>
 
                                     <v-card>
-                                        <v-tabs v-model="tab" show-arrows background-color="black" icons-and-text dark grow>
+                                        <v-tabs background-color="black" icons-and-text dark grow>
+
                                             <v-tabs-slider color="purple darken-4"></v-tabs-slider>
-                                            <v-tab v-for="i in tabs" :key="i">
-                                                <v-icon large>{{ i.icon }}</v-icon>
-                                                <div class="caption py-1">{{ i.name }}</div>
+                                            <v-tab >
+                                                <v-icon large>mdi-account</v-icon>
+                                                <div class="caption py-1">Login</div>
                                             </v-tab>
+
                                             <v-tab-item>
                                                 <v-card class="px-4">
                                                     <v-card-text>
@@ -125,35 +127,7 @@
                                                     </v-card-text>
                                                 </v-card>
                                             </v-tab-item>
-                                            <v-tab-item>
-                                                <v-card class="px-4">
-                                                    <v-card-text>
-                                                        <v-form ref="registerForm" v-model="valid" lazy-validation>
-                                                            <v-row>
-                                                                <v-col cols="12" sm="6" md="6">
-                                                                    <v-text-field v-model="firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
-                                                                </v-col>
-                                                                <v-col cols="12" sm="6" md="6">
-                                                                    <v-text-field v-model="lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
-                                                                </v-col>
-                                                                <v-col cols="12">
-                                                                    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-                                                                </v-col>
-                                                                <v-col cols="12">
-                                                                    <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                                                                </v-col>
-                                                                <v-col cols="12">
-                                                                    <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
-                                                                </v-col>
-                                                                <v-spacer></v-spacer>
-                                                                <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-                                                                    <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
-                                                                </v-col>
-                                                            </v-row>
-                                                        </v-form>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
+                                            
                                         </v-tabs>
                                     </v-card>
                                     
@@ -174,10 +148,10 @@
                                 
 
                                 <v-dialog
-                                v-model="dialog"
+                                v-model="dialog_register"
                                 persistent max-width="600px" 
                                 min-width="360px"
-                                @click:outside="dialog = false"
+                                @click:outside="dialog_register = false"
                                 >
                                     <template v-slot:activator="{ on, attrs }">
                                         <v-btn 
@@ -195,32 +169,11 @@
                                     <v-card>
                                         <v-tabs v-model="tab" show-arrows background-color="black" icons-and-text dark grow>
                                             <v-tabs-slider color="purple darken-4"></v-tabs-slider>
-                                            <v-tab v-for="i in tabs" :key="i">
-                                                <v-icon large>{{ i.icon }}</v-icon>
-                                                <div class="caption py-1">{{ i.name }}</div>
+                                            <v-tab>
+                                                <v-icon large>mdi-account-outline</v-icon>
+                                                <div class="caption py-1">Register</div>
                                             </v-tab>
-                                            <v-tab-item>
-                                                <v-card class="px-4">
-                                                    <v-card-text>
-                                                        <v-form ref="loginForm" v-model="valid" lazy-validation>
-                                                            <v-row>
-                                                                <v-col cols="12">
-                                                                    <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
-                                                                </v-col>
-                                                                <v-col cols="12">
-                                                                    <v-text-field v-model="loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                                                                </v-col>
-                                                                <v-col class="d-flex" cols="12" sm="6" xsm="12">
-                                                                </v-col>
-                                                                <v-spacer></v-spacer>
-                                                                <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
-                                                                    <v-btn x-large block :disabled="!valid" color=" primary" @click="validate"> Login </v-btn>
-                                                                </v-col>
-                                                            </v-row>
-                                                        </v-form>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
+                                           
                                             <v-tab-item>
                                                 <v-card class="px-4">
                                                     <v-card-text>
@@ -408,12 +361,9 @@
 
     data: () => ({
         dialog: false,
+        dialog_register: false,
 
-        tab: 0,
-        tabs: [
-            {name:"Login", icon:"mdi-account"},
-            {name:"Register", icon:"mdi-account-outline"}
-        ],
+   
         valid: true,
         
         firstName: "",
