9629e5514924aea301ddf3ce27a0a4c566ac6618
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2022-06-17 01:52:39 +0530

    meta data

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,18 @@
 <script>
 export default {
   name: "App",
-
+  metaInfo: {
+    title: 'Crispicy',
+    titleTemplate: '%s | Inventory & Sales management system',
+    htmlAttrs: {
+      lang: 'en-US'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'description', content: 'Are you a merchant or restaurant owner? This is a management system that will help merchant to keep track of sales, inventory, and other business information.' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  },
   data: () => ({
     //
   }),
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,6 +11,9 @@ import { firestorePlugin } from 'vuefire'
 
 Vue.use(firestorePlugin);
 Vue.config.productionTip = false;
+Vue.use(VueMeta, {
+  keyName: 'head'
+})
 
 const firebaseConfig = {
   apiKey: "AIzaSyALgjOUTpUbl-CegOPOUyUMu4fzUpRpCJ0",
